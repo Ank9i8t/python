59fc51f1071b20b62a30c35f699f7bb0598d2970
Anmol <78529845+Ank9i8t@users.noreply.github.com>
2021-08-11 22:13:10 +0530

    new string file
    new string file

---
+++ string.py
@@ -0,0 +1,50 @@
+a = 'Hello\\s world'
+b = " Hello's World " 
+# print(a)
+c = """
+This is multi line string.
+And It's ends here.
+"""
+# print(a+b)
+# print(c)
+# print(type(c))
+# # print(c)
+# # print(type(c))
+# print(len(b))
+# print(b[0:8])
+
+# print(b[-13:])
+
+
+
+
+
+
+# UPPER Case
+
+# print(a.upper())
+
+# Lower case
+# print(a.lower())
+# strip
+# print(len(b))
+# print(len(b.strip()))
+# print(len(b))
+#replace
+
+#split
+
+# print(type(c.split(' ')))
+
+#format Method and new approach in python3 =>f''
+
+first_name = 'Gautam'
+last_name = 'Rishi'
+full_name = 'my first name is {1} and surname is {0}'.format(last_name,first_name)
+# full_name = f'my first name is {first_name} and surname is {last_name}'
+# print(full_name)
+
+# Join method
+# arr_str = c.split(' ')
+# print(arr_str)
+# print(' '.join(arr_str))
