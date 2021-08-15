57833fcc36c2ec85ba2137f138209e7c65042937
Anmol <78529845+Ank9i8t@users.noreply.github.com>
2021-08-15 12:11:13 +0530

    if else and loops
    if else and loops

---
+++ if_loop.py
@@ -0,0 +1,72 @@
+# if else
+num = 5
+# if num % 2 == 0:
+#     print('divisible by 2')
+# else:
+#     print('not divisible by 2')
+
+# if num % 2 == 0:
+#     print('divided by 2')
+# elif num % 5 == 0:
+#     print('divided by 5')
+
+# else:
+#     print('not divisible by 2 and 5')
+
+num = 10
+# && -> and
+# || -> or
+
+# if (num % 2 == 0) and (num%5 == 0):
+#     print('num is divisble by 2 and 5')
+
+mystr = 'this is string'
+# mystr = 'e'
+# if 'a' in mystr:
+#     print('yes a hai')
+# else:
+#     print('nhi hain')
+
+
+# if 'a' !=  mystr:
+#     pass
+# else:
+#     print('yes a hain')
+# print('yaha aya')
+
+
+
+#while loop
+i=0
+# while i<len(mystr):
+#     print(mystr[i],end='')
+#     i+=1
+
+# num = 6
+# flag = 0
+# i = 2
+# while i<num//2:
+#     if num%i == 0:
+#         print('not prime')
+#         flag = 1
+#         break
+#     i+=1
+
+# if flag == 0:
+#     print('prime')
+
+
+# # for loop
+# n = 6
+# for i in range(n):
+#     print(i)
+n = 6
+# for i in range(n):
+#     for j in range(n-i-1):
+#         print('*',end='')
+#     print('\n')
+
+
+for i in range(n):
+    pass
+print(i)
