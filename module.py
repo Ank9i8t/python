7218052c38a720c243d0b6241906bbf4198606fb
Anmol <78529845+Ank9i8t@users.noreply.github.com>
2021-08-20 22:12:07 +0530

    module  file created
    module

---
+++ module.py
@@ -0,0 +1,51 @@
+
+
+#modules in python
+#A file containing a set of functions you want to include in your application
+from time import strftime
+import calculator as cal
+# from calculator import sum_three
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+a,b,c = 1,2,3
+# print('sum = ',sum_three(a,b,c))
+# print('divide = ',cal.division(a,b))
+
+# print(dir(cal))
+
+# print(type(cal.sum_three(a,b,c)))
+
+#creating and using modules
+
+#renaming a module
+
+#built-in module
+# datetime
+import datetime
+
+# print(dir(datetime))
+x = datetime.datetime.now()
+# print(x.strftime('%H:%M:%S'))
+#time
+
+# math
+# max()
+# min()
+# sum()
+# abs()
+# pow()
+
+# print(pow(5,2))
+import math
+# print(dir(math))
+print(math.sqrt(25))
+#dates modules -> strftime() method
+
+#math modules
+
+
+#dir() methods  
+
+#import from usecase
