2dfdfedf545279ad181b294c197649a92c51d31b
Anmol <78529845+Ank9i8t@users.noreply.github.com>
2021-08-22 22:02:49 +0530

    exception handling
    exception handling

---
+++ exception_handling.py
@@ -0,0 +1,41 @@
+#try except=catch
+# import xyz
+# try:
+#     a = 5/0
+#     # print(a)
+# except ArithmeticError as ex:
+#     print(f'ERROR1: {ex}')
+# except NameError as ex:
+#     print(f'ERROR2: {ex}')
+# except Exception as ex:
+#     print(f'ERROR3: {ex}')
+# else:
+#     print('try chl gya hain.')
+# finally:
+#     print('hmesa run krega')
+
+
+#else condition in try catch
+
+#raise error 
+
+# a = int(input())
+
+# if a< 0:
+#     raise print('input number is less than zero')
+
+# print('important')
+
+#Regex module
+import re
+
+print(dir(re))
+#methods
+
+#findall()
+
+#search()
+
+#split()
+
+#sub()
